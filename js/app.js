1a3823e34dfd2f318b1e691a4862f3663dc9933c
Bartosz Bujakowski <bbujakowski@efigence.com>
2017-08-11 12:03:01 +0200

    Adds review comments

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
+/*
+  Tu dostajesz błąd w konsoli:
+  http://foundation.zurb.com/forum/posts/21997-documentfoundation---uncaught-typeerror-undefined-is-not-a-function
+  zakładam, że skrypt został przeniesiony z index.html do app.js, zawsze po tego typu zmianach trzeba sprawdzic
+  czy wszystko działa.
+
+*/
 $(document).foundation()
